9aea750ad17371cb04fbd97642f22ba6a3f1e024
Olivier Cervello <olivier.cervello@gmail.com>
2016-04-28 21:45:00 -0500

    Add leaflet JS map + address locator

--- server.py
+++ server.py
@@ -1,14 +1,45 @@
 from __future__ import print_function  # In python 2.7
 from flask import Flask, render_template, request
+from googlemaps import googlemaps
+import json
+import logging
+import sys
+DEBUG = 'INFO'
+gmaps = googlemaps.Client("AIzaSyDqlY2dyOTrvcYWBtvsibdsetN14fJcnL4")
 app = Flask(__name__)
 
+###########
+# HELPERS #
+###########
+
+
+def log_config(log_level):
+    # check how your script can write to /var/log since there are some permission issues
+    logging.basicConfig(
+            stream=sys.stdout,
+            level=log_level,
+            format='%(asctime)s | %(levelname)s | %(filename)s | %(lineno)s | %(message)s'
+    )
+log_config(DEBUG)
+
+
+##########
+# ROUTES #
+##########
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        print("POST")
-        print(request.form['jobnumber'])
+    logging.info("Route: index")
     return render_template('index.html')
+
+
+@app.route('/address', methods=['POST'])
+def convert_address():
+    logging.info("Route: convert_address")
+    address = request.form['address']
+    j = gmaps.geocode(address)[0]['geometry']['location']
+    logging.info("Address: %s | Coordinates: %s" % (address, j))
+    return json.dumps(j)
 
 
 if __name__ == '__main__':
--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,31 @@
-$('#jobnumber').focusout(function(){
-      var jobnumber=$('#jobnumber').val()
+var map = L.map('map').setView([41.883734, -87.628858], 13);
+L.marker([41.883734, -87.628858]).addTo(map)
+      .bindPopup('Chicago Loop')
+      .openPopup();
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+$('#address').focusout(function(){
+      var address=$('#address').val()
+      console.log("Address: " + address)
       $.ajax({
-            url: '/',
+            url: '/address',
             data: $('form').serialize(),
             type: 'POST',
-            // success: function(response) {
-            //     //I need to some how be able to populate my table with the returned list called jobs;
-            // },
-            // error: function(error) {
-            //     alert(error);
-            // }
-        });
+            success: function(coordinates) {
+                  console.log("Coordinates: " + coordinates);
+                  var coordinates = JSON.parse(coordinates)
+                  var lat = coordinates.lat
+                  var lng = coordinates.lng
+                  console.log(lat)
+                  console.log(lng)
+                  map.panTo(new L.LatLng(lat, lng));
+                  L.marker([lat, lng]).addTo(map)
+            },
+            error: function(error) {
+                // alert(error);
+            }
+      });
 });
