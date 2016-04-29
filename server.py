f8fc3ddac06fbb5e783f022d45cd4ac3b390790f
Olivier Cervello <olivier.cervello@gmail.com>
2016-04-29 01:12:50 -0500

    Update

--- server.py
+++ server.py
@@ -4,13 +4,30 @@ from googlemaps import googlemaps
 import json
 import logging
 import sys
+import csv
+import pprint
 DEBUG = 'INFO'
 gmaps = googlemaps.Client("AIzaSyDqlY2dyOTrvcYWBtvsibdsetN14fJcnL4")
 app = Flask(__name__)
+pp = pprint.PrettyPrinter(indent=2)
+
+# Get csv data
+crime_csv = 'datasets/crimefinal.csv'
+hos_csv = 'datasets/hosfinal.csv'
+libraries_csv = 'datasets/librariesfinal.csv'
+school_csv = 'datasets/schoolfinal.csv'
+
+csv_paths = [crime_csv, hos_csv, libraries_csv, school_csv]
 
 ###########
 # HELPERS #
 ###########
+
+
+def read_csv(filepath):
+    with open(filepath, 'r') as f:
+        reader = csv.DictReader(f, fieldnames=['name', 'lat', 'lng'])
+        return list(reader)
 
 
 def log_config(log_level):
@@ -29,18 +46,39 @@ log_config(DEBUG)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    logging.info("Route: index")
+    logging.info("Route: /")
     return render_template('index.html')
 
 
 @app.route('/address', methods=['POST'])
 def convert_address():
-    logging.info("Route: convert_address")
     address = request.form['address']
-    j = gmaps.geocode(address)[0]['geometry']['location']
-    logging.info("Address: %s | Coordinates: %s" % (address, j))
-    return json.dumps(j)
+    response = gmaps.geocode(address)
+    logging.info(response)
+    coordinates = response[0]['geometry']['location']
+    logging.info("Route: /address | Address: %s | Coordinates: %s" % (address, coordinates))
+    return json.dumps(coordinates)
 
+
+@app.route('/get_csv_values', methods=['POST'])
+def get_csv_values():
+    # From the circle center latitute, longitude and the circle radius,
+    # get the points from each csv that are inside the circle
+    max_lat = request.json['lat']
+    lng = request.json['lng']
+    radius = request.json['radius']
+    markers = []
+    for path in csv_paths:
+        rows = read_csv(path)
+        for row in row:
+            # Check if inside the circle
+            markers.append({
+                'path': path.split('/')[-1]
+                'name': row['name'],
+                'lat': row['lat'],
+                'lng': row['lng']})
+    pp.pprint(markers)
+    return json.dumps(markers)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,51 @@
-var map = L.map('map').setView([41.883734, -87.628858], 13);
-L.marker([41.883734, -87.628858]).addTo(map)
+init_lat = 41.883734
+init_lng = -87.628858
+
+var map = L.map('map').setView([init_lat, init_lng], 15);
+var marker = L.marker([init_lat, init_lng]).addTo(map)
       .bindPopup('Chicago Loop')
       .openPopup();
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+var circle = L.circle([init_lat, init_lng], 200).addTo(map);
+var layer1 = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+})
+  .setOpacity(0.9)
+  .addTo(map);
+
+var layer2 = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+})
+  .setOpacity(0.6)
+  .addTo(map);
+var get_values = function(lat, lng, radius){
+      $.ajax({
+            url: '/get_csv_values',
+            data: JSON.stringify({'lat': lat, 'lng': lng, 'radius': radius}, null, '\t'),
+            contentType: 'application/json;charset=UTF-8',
+            type: 'POST',
+            success: function(list) {
+                  console.log(list)
 
+                  // for (item in list){
+                  //       path = item.path
+                  //       lat = item.lat
+                  //       lng = item.lng
+                  //       name = item.name
+                  //       L.marker([lat, lng]).addTo(map)
+                  //             .bindPopup(name)
+                  //             .openPopup();
+                  // };
+            },
+            error: function(error) {
+                alert(error);
+            }
+      });
+}
+var markers = get_values(init_lat, init_lng, 15)
 
+$("#slider").slider();
+$( "#slider" ).slider("option", "min", 200); // minimum radius in meters
+$( "#slider" ).slider("option", "max", 1000); // minimum radius in meters
 $('#address').focusout(function(){
       var address=$('#address').val()
       console.log("Address: " + address)
@@ -21,11 +60,186 @@ $('#address').focusout(function(){
                   var lng = coordinates.lng
                   console.log(lat)
                   console.log(lng)
-                  map.panTo(new L.LatLng(lat, lng));
-                  L.marker([lat, lng]).addTo(map)
+                  c = new L.LatLng(lat, lng)
+                  map.panTo(c, 15);
+                  marker.setLatLng(c)
+                  circle.setLatLng(c)
+                  markers = get_values(lat, lng, 15)
             },
             error: function(error) {
-                // alert(error);
+                alert(error);
             }
       });
 });
+$("#slider").on("slide", function( event, ui ) {
+      radius = ui.value
+      circle.setRadius(radius)
+      // markers = get_values(lat, lng, radius)
+});
+
+// $(document).ready(function () {
+//     var sevenDaysAgo;
+//     //initialize the leaflet map, set options and view
+//     var map = L.map('map', {
+//         zoomControl: false,
+//         scrollWheelZoom: false
+//     })
+//       .setView([40.705008, -73.995581], 15);
+
+//     var markers = new L.FeatureGroup();
+
+//     //add an OSM tileset as the base layer
+//     L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
+//         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+//     }).addTo(map);
+
+//     //call our getData() function.
+//     getData();
+
+//     //define a base icon
+//     var baseIcon = L.Icon.extend({
+//         options: {
+//             shadowUrl: 'img/shadow.png',
+
+//             iconSize: [32, 37], // size of the icon
+//             shadowSize: [51, 37], // size of the shadow
+//             iconAnchor: [16, 37], // point of the icon which will correspond to marker's location
+//             shadowAnchor: [25, 37],  // the same for the shadow
+//             popupAnchor: [1, -37] // point from which the popup should open relative to the iconAnchor
+//         }
+//     });
+
+//     //define agency icons based on the base icon
+//     var tlcIcon = new baseIcon({ iconUrl: 'static/images/taxi.png' });
+//     var dotIcon = new baseIcon({ iconUrl: 'static/images/dot.png' });
+//     var parksIcon = new baseIcon({ iconUrl: 'static/images/parks.png' });
+//     var buildingsIcon = new baseIcon({ iconUrl: 'static/images/buildings.png' });
+//     var nypdIcon = new baseIcon({ iconUrl: 'static/images/nypd.png' });
+//     var dsnyIcon = new baseIcon({ iconUrl: 'static/images/dsny.png' });
+//     var fdnyIcon = new baseIcon({ iconUrl: 'static/images/fdny.png' });
+//     var doeIcon = new baseIcon({ iconUrl: 'static/images/doe.png' });
+//     var depIcon = new baseIcon({ iconUrl: 'static/images/dep.png' });
+//     var dofIcon = new baseIcon({ iconUrl: 'static/images/dof.png' });
+//     var dcaIcon = new baseIcon({ iconUrl: 'static/images/dca.png' });
+//     var dohmhIcon = new baseIcon({ iconUrl: 'static/images/dohmh.png' });
+//     var hpdIcon = new baseIcon({ iconUrl: 'static/images/hpd.png' });
+
+
+//     function getData() {
+//         //get map bounds from Leaflet
+//         var bbox = map.getBounds();
+//         //map.removeLayer(markers);
+//         markers.clearLayers();
+//         //create a SODA-ready bounding box that looks like: topLeftLat,topLeftLon,bottomRightLat,bottomRightLon
+//         var sodaQueryBox = [bbox._northEast.lat, bbox._southWest.lng, bbox._southWest.lat, bbox._northEast.lng];
+
+//         //figure out what the date was 7 days ago
+//         var sevenDaysAgo = new Date();
+//         sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+//         $('#startDate').html(sevenDaysAgo.toDateString());
+
+//         function cleanDate(input) {
+//             return (input < 10) ? '0' + input : input;
+//         }
+
+//         //create a SODA-ready date string that looks like: 2014-11-01
+//         sevenDaysAgo = sevenDaysAgo.getFullYear()
+//                   + '-'
+//                   + cleanDate((sevenDaysAgo.getMonth() + 1))
+//                   + '-'
+//                   + cleanDate((sevenDaysAgo.getDate() + 1));
+
+//         //use jQuery's getJSON() to call the SODA API for NYC 311
+//         //concatenate sodaQueryBox and sevenDaysAgo to add a $where clause to the SODA endpoint
+//         $.getJSON(constructQuery(sevenDaysAgo, sodaQueryBox), function (data) {
+
+//                 console.log(data)
+//                       //iterate over each 311 complaint, add a marker to the map
+//                       for (var i = 0; i < data.length; i++) {
+
+//                           var marker = data[i];
+//                           var icon = getIcon(marker);
+
+//                           var markerItem = L.marker([marker.location.latitude, marker.location.longitude], { icon: icon });
+//                           markerItem.bindPopup(
+//                                           '<h4>' + marker.complaint_type + '</h4>'
+//                                           + (new Date(marker.created_date)).toDateString()
+//                                           + ((marker.incident_address != null) ? '<br/>' + marker.incident_address : '')
+//                                     );
+//                           markers.addLayer(markerItem);
+//                       }
+//             //.addTo(map);
+//                       map.addLayer(markers);
+
+//                   })
+//     }
+
+//     function constructQuery(sevenDaysAgo, sodaQueryBox) {
+//         var originalstr = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$select=location,closed_date,complaint_type,street_name,created_date,status,unique_key,agency_name,due_date,descriptor,location_type,agency,incident_address&$where=created_date>'"
+//                   + sevenDaysAgo
+//                   + "' AND within_box(location,"
+//                   + sodaQueryBox
+//                   + ")&$order=created_date desc"
+
+//         var agency = $( "#nycAgency" ).val();
+//         var conditiion = $("#conditions_list").val();
+//         if (agency.length != 0 && agency != "All") {
+//             originalstr = originalstr + "&agency=" + agency;
+//         }
+//         if (conditiion.length != 0 && conditiion != "All") {
+//             originalstr = originalstr + "&complaint_type=" + conditiion;
+//         }
+
+//         console.log(originalstr);
+
+//         return originalstr;
+//     }
+
+
+// //     function getIcon(thisMarker) {
+
+// //         switch (thisMarker.agency) {
+// //             case 'TLC':
+// //                 return tlcIcon;
+// //             case 'DOT':
+// //                 return dotIcon;
+// //             case 'DPR':
+// //                 return parksIcon;
+// //             case 'DOB':
+// //                 return buildingsIcon;
+// //             case 'NYPD':
+// //                 return nypdIcon;
+// //             case 'DSNY':
+// //                 return dsnyIcon;
+// //             case 'FDNY':
+// //                 return fdnyIcon;
+// //             case 'DOE':
+// //                 return doeIcon;
+// //             case 'DEP':
+// //                 return depIcon;
+// //             case 'DOF':
+// //                 return dofIcon;
+// //             case 'DCA':
+// //                 return dcaIcon;
+// //             case 'DOHMH':
+// //                 return dohmhIcon;
+// //             case 'HPD':
+// //                 return hpdIcon;
+// //             default:
+// //                 return new L.Icon.Default();
+// //         }
+// //     }
+
+// //     map.on('dragend', function (e) {
+// //         getData();
+// //     });
+
+// //     $('#nycAgency').on("change", function () {
+// //         getData();
+// //     });
+
+// //     $("#conditions_list").on('change keyup paste', function () {
+// //         getData();
+// //     });
+// // });
